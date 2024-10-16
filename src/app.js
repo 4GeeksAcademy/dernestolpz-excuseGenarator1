4acc2f0d13b88b61642998a252533bbc3f9ec140
dernestolpz <dernesto.lopez@gmail.com>
2024-10-16 11:19:59 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["El gato", "la niñera", "La abuela", "El pez", "El niño"];
+  let action = [
+    "se comio un pan",
+    "pisó al gato",
+    "dibujó un mar",
+    "se murió",
+    "estudió matematicas"
+  ];
+  let place = [
+    "en el bosque",
+    "en la casa",
+    "en el metro",
+    "en la piscina",
+    "en el avión"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomPlace = Math.floor(Math.random() * place.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] + " " + action[randomAction] + " " + place[randomPlace];
 };
